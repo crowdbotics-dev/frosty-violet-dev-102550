84476c2b13d6e145f4e3464f2ee8f29b16b29310
Crowdbotics <engineering@crowdbotics.com>
2023-07-18 11:22:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "frosty-violet-dev-102550",
+  url: "https://frosty-violet-dev-102550.botics.co",
+  api: "https://frosty-violet-dev-102550.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
